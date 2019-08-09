d20496eb3c568dc94a07fb788bb6c66d8bf0dce3
Sujil Anto <sujilnt@gmail.com>
2019-08-09 23:46:53 +0100

    adding some more changes

--- src/Server/server.js
+++ src/Server/server.js
@@ -1,21 +1,22 @@
-import express from 'express'
-import expressStaticGzip from 'express-static-gzip'
-const app = express()
+import express from "express";
+import expressStaticGzip from "express-static-gzip";
+//import graphqlHTTP from "express-graphql";
+const app = express();
 
-app.get('/api', (req, res) => {
-  res.send({ data: 'hi sujil' })
-})
-let testdata = { '1': 1 }
+app.get("/api", (req, res) => {
+	res.send({ data: "hi sujil" });
+});
+let testdata = { "1": 1 };
 var options = {
-  enableBrotli: true,
-  orderPreference: ['br', 'gz'],
-  setHeaders: function (res) {
-    res.setHeader('Cache-Control', 'public, max-age=31536000')
-  }
-}
+	enableBrotli: true,
+	orderPreference: ["br", "gz"],
+	setHeaders: function(res) {
+		res.setHeader("Cache-Control", "public, max-age=31536000");
+	},
+};
 
-console.log('testingsdsdvsvsdvdsvs', { ...testdata })
-app.use(expressStaticGzip('./ClientBundle', options))
-app.listen('9001', () => {
-  console.log('App started at port number 9001')
-})
+console.log("testingsdsdvsvsdvdsvs", { ...testdata });
+app.use(expressStaticGzip("./ClientBundle", options));
+app.listen("9001", () => {
+	console.log("App started at port number 9001");
+});
