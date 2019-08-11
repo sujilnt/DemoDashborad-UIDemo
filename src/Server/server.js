92e27340ca9a592962f99edaf1e79177ed87aee8
Sujil Anto <sujilnt@gmail.com>
2019-08-11 23:59:29 +0100

    refactoring express

--- src/Server/server.js
+++ src/Server/server.js
@@ -1,6 +1,8 @@
 import express from 'express'
 import expressStaticGzip from 'express-static-gzip'
+import { connect } from './utils/db'
 
+const PORT_NUM = 9015
 const app = express()
 
 var options = {
@@ -11,7 +13,22 @@ var options = {
   }
 }
 
-app.use(expressStaticGzip('./ClientBundle', options))
-app.listen('9001', () => {
-  console.log('App started at port number 9001')
+app.get('/', expressStaticGzip('./ClientBundle', options), (req, res) => {
+  console.log(req.body)
+  res.send(req.body)
 })
+
+// app.use(expressStaticGzip('./ClientBundle', options))
+
+export const start = async () => {
+  try {
+    await connect()
+    app.listen(PORT_NUM, () => {
+      console.log(`App staeted at port number ${PORT_NUM}`)
+    })
+  } catch (e) {
+    console.error(e)
+  }
+}
+
+start()
---
+++ src/Server/utils/db.js
@@ -0,0 +1,13 @@
+// import mongoose and setting up connection to mongoDB
+import mongoose from 'mongoose'
+const mongoDB_URL = 'mongodb://localhost:27017/Dashboadv1'
+export const connect = () => {
+  return mongoose.connect(
+    mongoDB_URL,
+    {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    }
+  )
+}
