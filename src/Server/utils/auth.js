56f48e0a423d915d2a7c60e5576f84b29f72e8b9
SUJIL ANTO THOMAS <sujilnt@gmail.com>
2019-08-19 20:30:05 +0100

    updating keys and added bearer token

--- src/Server/utils/auth.js
+++ src/Server/utils/auth.js
@@ -1,11 +1,12 @@
 import jwt from "jsonwebtoken";
+import User from "../Resources/Login-SignUP/User.model";
 import { DEV_jwt, DEV_jwtExp } from "babel-dotenv";
 const secrets = {
 	jwt: DEV_jwt,
 	jwtExp: DEV_jwtExp,
 };
 export const newToken = user => {
-	return jwt.sign({ user }, secrets.jwt, {
+	return jwt.sign({ uid: user.uid }, secrets.jwt, {
 		expiresIn: secrets.jwtExp,
 	});
 };
@@ -17,3 +18,30 @@ export const verifyToken = token =>
 			resolve(payload);
 		});
 	});
+
+export const protect = async (req, res, next) => {
+	const bearer = req.headers.authorization;
+	if (!bearer || !bearer.startsWith("Bearer ")) {
+		return res.status(401).end();
+	}
+	// "Bearer  token"
+	const token = bearer.split("Bearer ")[1].trim();
+	let payload;
+	try {
+		payload = await verifyToken(token);
+	} catch (e) {
+		return res.status(401).end();
+	}
+
+	const user = await User.findById(payload.id)
+		.select("-password")
+		.lean()
+		.exec();
+
+	if (!user) {
+		return res.status(401).end();
+	}
+
+	req.user = user;
+	next();
+};
--- src/Server/utils/db.js
+++ src/Server/utils/db.js
@@ -1,6 +1,6 @@
 // import mongoose and setting up connection to mongoDB
 import mongoose from "mongoose";
-const mongoDB_URL = "mongodb://localhost:27017/Dashboadv1";
+const mongoDB_URL = "mongodb://localhost:27017/Dashboadv2";
 export const connect = () => {
 	return mongoose.connect(mongoDB_URL, {
 		useNewUrlParser: true,
