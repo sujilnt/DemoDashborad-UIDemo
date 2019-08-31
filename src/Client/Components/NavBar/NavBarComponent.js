24516970824b6f030c3048e67b30f241fbec0c22
SUJIL ANTO THOMAS <sujilnt@gmail.com>
2019-08-31 22:11:54 +0100

    updating the data

--- src/Client/Components/NavBar/NavBarComponent.js
+++ src/Client/Components/NavBar/NavBarComponent.js
@@ -1,10 +1,7 @@
 import React from "react";
-import {} from "react-redux"
-import { NavbarGroup, NavbarHeading, NavbarDivider, Icon,Button, Navbar, Classes, Alignment } from "@blueprintjs/core";
+import { NavbarGroup,NavbarHeading, NavbarDivider, Icon,Button, Navbar, Classes, Alignment } from "@blueprintjs/core";
 import {useSelector} from "react-redux";
 import SideBar from "../SideBar/SideBar";
-import TreeComponent from "../Tree/Tree";
-import TreeContainer from "../Tree/TreeContainer";
 
 const NavbarComponent = (props) =>{
 	console.log("Navbar props",props);
@@ -14,21 +11,21 @@ const NavbarComponent = (props) =>{
 			<Navbar className="main-page_nav">
 				<NavbarGroup align={Alignment.LEFT} className="main-page_nav">
 					<div style={{"margin": "0 auto", "width": "200px", "textAlign": "center"}}>
-						<NavbarHeading>
-							<span>IOT Analytics &nbsp;</span>
-							<Icon icon={"chart"} intent={"primary"} iconSize={20} />
+						<NavbarHeading className={"contentCenter alignCenter"}>
+							<span style={{"fontWeight": "bolder","fontSize": "18"}}>IOT Analytics &nbsp;</span>
+							<Icon icon={"chart"} intent={"primary"} iconSize={24}/>
 						</NavbarHeading>
 					</div>
 				</NavbarGroup>
 				<NavbarGroup align={Alignment.RIGHT} className="main-page_nav">
 					<NavbarHeading>Welcome {intialState.name}!</NavbarHeading>
-					<NavbarDivider />
+					<NavbarDivider/>
 					<Button className={Classes.MINIMAL} icon="notifications" text="Notifications"/>
 					<Button className={Classes.MINIMAL} icon="log-out" text="logout"/>
 				</NavbarGroup>
 			</Navbar>
 			<div style={{"width": 300}}>
-				<SideBar location = {props.location}/>
+				<SideBar location={props.location}/>
 			</div>
 
 		</div>
