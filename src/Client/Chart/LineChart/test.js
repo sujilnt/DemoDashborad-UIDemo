a4b135e0a42d2a9ead34cf350cca3b2639aa904f
SUJIL ANTO THOMAS <sujilnt@gmail.com>
2019-09-03 23:36:58 +0100

    adding some changes to graph x axis y axis

--- src/Client/Chart/LineChart/test.js
+++ src/Client/Chart/LineChart/test.js
@@ -11,7 +11,7 @@ class LineChart extends PureComponent{
         let width = document.querySelector(`.${this.props.containerclassName}`).clientWidth;
         let height = document.querySelector(`.${this.props.containerclassName}`).clientHeight;
         console.log("height,weight", width,height);
-        let margin = {top: 10, right: 0, bottom: 20, left: 30};
+        let margin = {top: 10, right: 30, bottom: 20, left: 30};
         const innerWidth = width - margin.left - margin.right;
         const innerHeight = height - margin.top - margin.bottom;
 
@@ -19,15 +19,15 @@ class LineChart extends PureComponent{
         console.log(chartdata);
         const svg = select("#lc-svg")
             .append("svg")
-            .attr("width","100%")
-            .attr("height","100%")
-            //.attr('viewBox','-0 -0 '+Math.min(width,height) +' '+Math.min(width,height))
-            .attr('viewBox',`-25 10 ${Math.min(width,height)+0} ${Math.min(width,height)+5}`)
+            .attr("width",innerWidth)
+            .attr("height",innerHeight)
+            //.attr('viewBox','-30 -10 '+Math.min(width,height) +' '+Math.min(width,height))
+            .attr('viewBox',`-30 -10 ${Math.min(width,height)} ${Math.min(width,height)+40 }`)
+            //.attr('viewBox',`-30 -10 100% 100%`)
             .attr('preserveAspectRatio','xMinYMin meet')
             .append("g")
-            .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
-            .attr("width",innerWidth).attr("height",innerHeight)
-        ;
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
         const xextent = extent(chartdata, d=> {
             let date = new Date(timeParse(d.time));
             return date;
@@ -73,7 +73,7 @@ class LineChart extends PureComponent{
         xAxisG.append('text')
             .attr('class', 'axis-label')
             .attr('x', innerWidth / 2)
-            .attr('y', 34)
+            .attr('y', 40)
             .text("⟵ date ⟶");
         console.log(innerHeight,innerWidth);
         yAxisG.append('text')
@@ -81,7 +81,8 @@ class LineChart extends PureComponent{
             .attr('x', innerHeight-20)
             .attr('y', 53)
             .attr('transform', `rotate(90)`)
-            .text("temperature in Celsius ⟵ ");
+            //.style('text-anchor', 'middle')
+            .text("⟵ temperature in Celsius");
 
         xAxisG.call(xAxis);
         yAxisG.call(yAxis);
