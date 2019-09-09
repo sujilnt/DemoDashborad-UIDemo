d1c17e318537d077a9ec5cd6782e8ce23bfa2b83
SUJIL ANTO THOMAS <sujilnt@gmail.com>
2019-09-09 03:41:05 +0100

    updating service worker ,js

--- src/sw-dev.js
+++ src/sw-dev.js
@@ -1,5 +1,36 @@
-const version = 3;
+const version = new Date();
+// precaching the all ss
+workbox.precaching.precacheAndRoute(self.__precacheManifest,{
+    ignoreURLParametersMatching: [/.*/]
+});
 
-workbox.precaching.precacheAndRoute(self.__precacheManifest);
+// caching all the data
+workbox.routing.registerRoute(
+    /\.(?:js|css|html)$/,  (new workbox.strategies.NetworkFirst())
+);
 
-//workbox.routing.registerRoute("#/", new workbox.strategies.StaleWhileRevalidate());
+// caching all the sensor informating in the cache
+workbox.routing.registerRoute(
+    "http://localhost:9001/api/sensor/" ,(new workbox.strategies.NetworkFirst())
+);
+/*
+const matchCb =({url,event})=>{
+    console.log(url, "SERVICE WORKER URL !!",url.pathname.indexOf("/api/sensor/events/")>=0);
+  return(url.pathname.indexOf("/api/sensor/events/")>=0);
+};
+
+//workbox.routing.registerRoute(matchCb,(new workbox.strategies.NetworkFirst()));
+
+self.addEventListener("fetch",(event)=>{
+   const responseUrl = new URL(event.request.url);
+   if(responseUrl.pathname.indexOf("/api/sensor/events/")>=0){
+       const network_First = new workbox.strategies.NetworkFirst({
+           cacheName: "sujil",
+           matchOptions:{
+               ignoreSearch:true
+           }
+       });
+   }
+});
+
+ */
